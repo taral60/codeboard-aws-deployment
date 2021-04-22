2f0b890812739aa63e2859b8eeb65f39df792369
taral <taral@DESKTOP-5KRPR6M>
2021-04-21 20:49:42 -0600

    Initial Commit

---
+++ src/main/java/com/codeboard/codeboardaws/controller/PersonController.java
@@ -0,0 +1,14 @@
+package com.codeboard.codeboardaws.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+	
+	@RequestMapping("/getname")
+	public String getName() {
+		return "group3";
+	}
+
+}
